1beef2395fd5528c9b2aef9747ff0172f3ffa415
Frederikke Grøndal Schjøtt <eaafgs@students.eaaa.dk>
2019-03-12 12:23:21 +0100

    Første udkast

---
+++ script/script.js
@@ -0,0 +1,11 @@
+function nextOne() {
+  document.getElementById('step-two').classList.add("horiz");
+  document.getElementById('step-one').classList.add('horizontal')
+}
+
+function nextTwo() {
+  document.getElementById('step-two').classList.add("horizzz");
+  document.getElementById('step-three').classList.add('hor')
+}
+
+function submitThree() {}
